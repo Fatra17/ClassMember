640aa7fc6b646bd42f1987942bc05f2493646824
Fatra17 <fatra170607@gmail.com>
2026-04-25 10:00:33 +0700

    commit one

---
+++ LatihanMemberClass/PlaneTiket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    internal class PlaneTiket
+    {
+        private string _origin;
+        private string _direction;
+        private double _cost;
+        private double _currency;
+    }
+}
